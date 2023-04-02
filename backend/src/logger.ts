c1903f1a0f8ccd3bcb9b96efea9f10a2ac12843b
anthony <anthony.sherrill@code.berlin>
2023-04-02 15:10:14 +0200

    Implement Logger and configure it nice

--- backend/src/logger.ts
+++ backend/src/logger.ts
@@ -3,7 +3,14 @@ import * as winston from 'winston';
 // create a new logger instance
 const logger = winston.createLogger({
     level: 'info',
-    format: winston.format.json(),
+    format: winston.format.combine(
+        winston.format.timestamp({
+            format: 'YYYY-MM-DD HH:mm:ss'
+        }),
+        winston.format.printf(({ level, message, timestamp }) => {
+            return `${timestamp} ${level}: ${message}`;
+        })
+    ),
     defaultMeta: { service: 'my-app' },
     transports: [
         new winston.transports.File({ filename: 'error.log', level: 'error' }),
