6e85d11c09b5421f4ea36ab0727b7c96db395375
Darjusch Schrand <Darjusch Schrand>
2023-04-10 22:56:17 +0200

    kitaList from routes to function

--- backend/src/kitas/kitaList.ts
+++ backend/src/kitas/kitaList.ts
@@ -1,36 +1,50 @@
+import axios from "axios";
 import { Kita } from "../type";
+import logger from "../logger";
 
 /**
- * Transforms and translates all the json objects from the kita-navigator API response
- * @param JSON input - The JSON response from the kita-navigator API
+ * Finds nearby kita centers based on latitude and longitude
+ * @param {number} lat - The latitude coordinate
+ * @param {number} lon - The longitude coordinate
  * @returns {Kita[]} - A list of kita centers in JSON format
  */
-export function kitaList(input: any): Kita[] {
-  const facilities = input.einrichtungen.map((facility: any) => {
-    const availability: { [key: string]: boolean } = {};
-    facility.freiplatzstatus.forEach((status: any) => {
-      availability[status.gueltigAb] = status.plaetzeVerfuegbar;
-    });
-
-    const facilityObj: Kita = {
-      uuid: facility.id.toString(),
-      name: facility.name,
-      number: facility.nummer,
-      coordinates: {
-        lat: facility.geokoordinate.lat,
-        lng: facility.geokoordinate.lon,
-        dist: facility.geokoordinate.entfernung,
-      },
-      address: {
-        street: facility.adresse.strasse,
-        houseNumber: facility.adresse.hausnummer,
-        zip: facility.adresse.plz,
-        city: facility.adresse.ort,
-      },
-      availability,
-      imageUrl: 'https://kita-navigator.berlin.de' + facility.vorschaubild.url,
-    };
-    return facilityObj;
-  });
-  return facilities;
+export async function kitaList(req: any, res: any) {
+    try{
+        let kitas = await axios.get(
+            `https://kita-navigator.berlin.de/api/v1/kitas/umkreissuche?entfernung=500&lat=${req.params.lat}&lon=${req.params.lon}&seite=0&max=4000`
+          );
+          const facilities = kitas.data.einrichtungen.map((facility: any) => {
+            const availability: { [key: string]: boolean } = {};
+            facility.freiplatzstatus.forEach((status: any) => {
+              availability[status.gueltigAb] = status.plaetzeVerfuegbar;
+            });
+        
+            const facilityObj: Kita = {
+              uuid: facility.id.toString(),
+              name: facility.name,
+              number: facility.nummer,
+              coordinates: {
+                lat: facility.geokoordinate.lat,
+                lng: facility.geokoordinate.lon,
+                dist: facility.geokoordinate.entfernung,
+              },
+              address: {
+                street: facility.adresse.strasse,
+                houseNumber: facility.adresse.hausnummer,
+                zip: facility.adresse.plz,
+                city: facility.adresse.ort,
+              },
+              availability,
+              imageUrl: 'https://kita-navigator.berlin.de' + facility.vorschaubild.url,
+            };
+            return facilityObj;
+          });
+          logger.info(`Retrieved ${facilities.length} kitas.`);
+          res.status(200).send(facilities);
+    }
+    catch(err: any){
+        logger.error(err.message);
+        res.status(500).json({"error": "Something went wrong"});
+    }
+    
 }
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -2,32 +2,17 @@ import express = require('express');
 import einzelBenachrichtigungHandler from './anmeldungen/einzel';
 import arealBenachrichtigungHandler from './anmeldungen/areal';
 import serviceAnmeldungHandler from './anmeldungen/service';
-import { kitaList, kitaDetail } from './controller';
 import logger from './logger';
 import { locationService } from './location-service';
 import { locationValidator } from './location-service/validator';
+import { kitaList } from './kitas/kitaList';
+import { kitaDetail } from './kitas/kitaDetail';
 
 const router = express.Router();
 const axios = require('axios');
 
-/**
- * Finds nearby kita centers based on latitude and longitude
- * @param {number} lat - The latitude coordinate
- * @param {number} lon - The longitude coordinate
- * @returns {Kita[]} - A list of kita centers in JSON format
- */
-router.get('/kitas/:lat/:lon', async (req, res) => {
-  try {
-    let kitas = await axios.get(
-      `https://kita-navigator.berlin.de/api/v1/kitas/umkreissuche?entfernung=500&lat=${req.params.lat}&lon=${req.params.lon}&seite=0&max=4000`
-    );
-    const result = kitaList(kitas.data);
-    logger.info(`Retrieved ${result.length} kitas.`);
-    res.send(result);
-  } catch (err: any) {
-    logger.error(err.message);
-  }
-});
+
+router.get('/kitas/:lat/:lon', kitaList);
 
 /**
  * Returns details for a specific kita center
