a25268e8158e8dd894e752810d27e5073fcb4908
anthony <anthony.sherrill@code.berlin>
2023-04-11 18:28:50 +0200

    Implemented logic to iterate over kitaDetails

--- backend/src/scripts/iterateOverKitas.ts
+++ backend/src/scripts/iterateOverKitas.ts
@@ -1,11 +1,20 @@
-import { Kita } from '../type';
+import axios from "axios";
+import { Kita } from "../type";
+import { kitaDetail } from "../controller";
+import logger from "../logger";
 
-const kitas: Kita[] = require('../../data/kitas_berlin.json');
+const kitas: Kita[] = require("../../data/kitas_berlin.json");
 
-function printKitaNames(): void {
-    for (const kita of kitas) {
-        console.log(kita);
-    }
+async function printKitaNames(): Promise<void> {
+  for (const kita of kitas) {
+    console.log(kita.uuid);
+    let kita_res = await axios.get(
+      `https://kita-navigator.berlin.de/api/v1/kitas/${kita.uuid}`
+    );
+    logger.info(`Retrieved details for Kita with uuid ${kita.uuid}.`);
+    const result = kitaDetail(kita_res.data);
+    logger.info(result);
+  }
 }
 
 printKitaNames();
