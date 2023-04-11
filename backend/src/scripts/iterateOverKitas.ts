b3fd92e5f87c8225accdb9c7fb9d0c914a54f7e1
anthony <anthony.sherrill@code.berlin>
2023-04-11 22:08:38 +0200

    Added retry logic & close connection when saveKitaDetailsToDB is done

--- backend/src/scripts/iterateOverKitas.ts
+++ backend/src/scripts/iterateOverKitas.ts
@@ -6,25 +6,38 @@ import logger from "../logger";
 const kitas: Kita[] = require("../../data/kitas_berlin.json");
 const mongoose = require("mongoose");
 
+async function fetchKitaWithRetry(uuid: string, retries = 6): Promise<any> {
+  try {
+    const response = await axios.get(
+      `https://kita-navigator.berlin.de/api/v1/kitas/${uuid}`
+    );
+    return response;
+  } catch (error) {
+    if (retries > 0 && error.code === "ECONNRESET") {
+      console.log(
+        `Error with uuid ${uuid}: ${error.message}. Retrying in 10 seconds...`
+      );
+      await new Promise((resolve) => setTimeout(resolve, 10000));
+      return await fetchKitaWithRetry(uuid, retries - 1);
+    } else {
+      throw error;
+    }
+  }
+}
+
 async function saveKitaDetailsToDB(): Promise<void> {
   try {
     const dbUrl =
       "mongodb+srv://kpz-dev:Nix123456@kitaplatzzentralecluste.dz80v4r.mongodb.net/?retryWrites=true&w=majority";
-    mongoose
+    await mongoose
       .connect(dbUrl, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
       })
       .then(async () => {
         console.log("DB connected");
-        // current kita tracken
-        // if connection reset
-        // start again at this kita
         for (const kita of kitas) {
-          // console.log(kita.uuid);
-          let kitaRes = await axios.get(
-            `https://kita-navigator.berlin.de/api/v1/kitas/${kita.uuid}`
-          );
+          let kitaRes = await fetchKitaWithRetry(kita.uuid);
           console.log(`Retrieved details for Kita with uuid ${kita.uuid}.`);
           const kitaDetail = transformExternalKitaDetailsToKitaDetails(
             kitaRes.data
@@ -39,8 +52,16 @@ async function saveKitaDetailsToDB(): Promise<void> {
       })
       .catch((err: any) => console.log(`MongoDB connection error: ${err}`));
   } catch (err) {
-    console.log("Something went wrong");
-    console.log(err);
+    logger.error("Something went wrong:", err);
+  } finally {
+    await mongoose.connection
+      .close()
+      .then(() => {
+        console.log("MongoDB connection closed");
+      })
+      .catch((error: any) => {
+        console.log("Error while closing MongoDB connection:", error);
+      });
   }
 }
 
