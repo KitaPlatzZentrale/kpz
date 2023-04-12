aa90d971964fceed57f06562048c4fc7b1d12355
Darjusch Schrand <Darjusch Schrand>
2023-04-12 15:02:34 +0200

    extracted database connection to func

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,27 +1,19 @@
-import express = require('express');
-import routes = require('./routes');
-import cors from 'cors';
-import logger from './logger';
-const mongoose = require('mongoose');
+import express = require("express");
+import routes = require("./routes");
+import cors from "cors";
+import logger from "./logger";
+import { connectToDatabase } from "./database";
 
 const app = express();
 
-require("dotenv").config();
-
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
 app.use("", routes);
 
-const dbUrl = 'mongodb+srv://kpz-dev:Nix123456@kitaplatzzentralecluste.dz80v4r.mongodb.net/?retryWrites=true&w=majority'
-mongoose.connect(dbUrl, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connected!'))
-.catch((err: any) => console.log(`MongoDB connection error: ${err}`));
-
+connectToDatabase();
 
 app.listen(3000, () => {
+  console.log("Server started on port 3000");
   logger.info("Server started on port 3000");
 });
