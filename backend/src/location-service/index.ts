bee7c30fb61a31ffc3d25149c0ff757c1ed2af1f
Darjusch Schrand <Darjusch Schrand>
2023-04-12 15:03:28 +0200

    S3 bucket string into .env file

--- backend/src/location-service/index.ts
+++ backend/src/location-service/index.ts
@@ -1,28 +1,36 @@
-import { NextFunction } from 'express';
-import haversine from 'haversine-distance';
-import { Kita } from '../type';
+require("dotenv").config();
+import { NextFunction } from "express";
+import haversine from "haversine-distance";
+import { Kita } from "../types";
 
 const axios = require("axios");
 
 export async function locationService(req: any, res: any, next: NextFunction) {
-  try {
-    const {lat, lon, radius} = req.body
-
+  try {
+    const { lat, lon, radius } = req.body;
+    const S3_BUCKET = process.env.S3_BUCKET;
+    if (!S3_BUCKET) {
+      throw "\n\n\nYOU NEED TO ADD THE S3_BUCKET STRING TO YOUR .env FILE IN THE ROOT FOLDER\n\n\n";
+    }
     // 1. Get the Kita List
-    let kitaList = await axios.get('https://kpzbucket.s3.eu-central-1.amazonaws.com/kitas_berlin.json')
+    let kitaList = await axios.get(S3_BUCKET);
     // 2. Calculate the distance
-    let kitasInRadius: Kita[] = []
+    let kitasInRadius: Kita[] = [];
     kitaList.data.map((kita: Kita) => {
-      const distance = haversine({lat, lon}, {lat: kita.coordinates.lat, lon: kita.coordinates.lng}) / 1000
-      if(distance <= radius) {
-        kita.coordinates.dist = distance
-        kitasInRadius.push(kita)
+      const distance =
+        haversine(
+          { lat, lon },
+          { lat: kita.coordinates.lat, lon: kita.coordinates.lng }
+        ) / 1000;
+      if (distance <= radius) {
+        kita.coordinates.dist = distance;
+        kitasInRadius.push(kita);
       }
-    })
+    });
     // 3. Send the Kita List with the distance
-    return res.send(kitasInRadius)
+    return res.send(kitasInRadius);
   } catch (err: any) {
-    console.log(err)
+    console.log(err);
     return res.status(500).json({ error: "Something went wrong" });
   }
 }
