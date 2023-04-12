7f27af42e293c9d90a70bb327881c5b69ad6297e
Darjusch Schrand <Darjusch Schrand>
2023-04-12 15:03:49 +0200

    replaced db connection and closing into function

--- backend/src/scripts/iterateOverKitas.ts
+++ backend/src/scripts/iterateOverKitas.ts
@@ -1,10 +1,10 @@
-import { Kita } from "../type";
+import { Kita } from "../types";
 import { KitaDetailModel } from "../models";
 import axios from "axios";
 import { transformExternalKitaDetailsToKitaDetails } from "../kitas/getKitaDetails";
 import logger from "../logger";
+import { closeDatabaseConnection, connectToDatabase } from "../database";
 const kitas: Kita[] = require("../../data/kitas_berlin.json");
-const mongoose = require("mongoose");
 
 async function fetchKitaWithRetry(uuid: string, retries = 6): Promise<any> {
   try {
@@ -27,41 +27,23 @@ async function fetchKitaWithRetry(uuid: string, retries = 6): Promise<any> {
 
 async function saveKitaDetailsToDB(): Promise<void> {
   try {
-    const dbUrl =
-      "mongodb+srv://kpz-dev:Nix123456@kitaplatzzentralecluste.dz80v4r.mongodb.net/?retryWrites=true&w=majority";
-    await mongoose
-      .connect(dbUrl, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      })
-      .then(async () => {
-        console.log("DB connected");
-        for (const kita of kitas) {
-          let kitaRes = await fetchKitaWithRetry(kita.uuid);
-          console.log(`Retrieved details for Kita with uuid ${kita.uuid}.`);
-          const kitaDetail = transformExternalKitaDetailsToKitaDetails(
-            kitaRes.data
-          );
-          logger.info(kitaDetail);
-          await KitaDetailModel.findOneAndUpdate(
-            { uuid: kitaDetail.uuid },
-            kitaDetail,
-            { upsert: true }
-          );
-        }
-      })
-      .catch((err: any) => console.log(`MongoDB connection error: ${err}`));
+    await connectToDatabase();
+    for (const kita of kitas) {
+      let kitaRes = await fetchKitaWithRetry(kita.uuid);
+      const kitaDetail = transformExternalKitaDetailsToKitaDetails(
+        kitaRes.data
+      );
+      logger.info(kitaDetail);
+      await KitaDetailModel.findOneAndUpdate(
+        { uuid: kitaDetail.uuid },
+        kitaDetail,
+        { upsert: true }
+      );
+    }
   } catch (err) {
     logger.error("Something went wrong:", err);
   } finally {
-    await mongoose.connection
-      .close()
-      .then(() => {
-        console.log("MongoDB connection closed");
-      })
-      .catch((error: any) => {
-        console.log("Error while closing MongoDB connection:", error);
-      });
+    closeDatabaseConnection();
   }
 }
 
