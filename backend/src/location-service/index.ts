18679f641251109d9ec3418f761c4c75c984476a
Darjusch Schrand <Darjusch Schrand>
2023-04-19 20:14:46 +0200

    Fixed bug and replaced S3_bucket with mongo db

--- backend/src/location-service/index.ts
+++ backend/src/location-service/index.ts
@@ -2,25 +2,24 @@ import haversine from "haversine-distance";
 import logger from "../services/logger";
 import { Kita } from "../types";
 import { NextFunction } from "express";
+import { connectToDatabase } from "../services/database";
+import { KitaDetailModel } from "../models";
 
 require("dotenv").config();
 
-const axios = require("axios");
-
 export async function locationService(req: any, res: any, next: NextFunction) {
   try {
-    const { lat, lon} = req.params;
+    const { lat, lon } = req.params;
     const radius = req.params.radius || 2.5;
-    const page = parseInt(req.params.page)
-    const size = parseInt(req.params.size)
-    const S3_BUCKET = process.env.S3_BUCKET;
+    const page = parseInt(req.params.page);
+    const size = parseInt(req.params.size);
 
-    if (!S3_BUCKET) {
-      throw "\n\n\nYOU NEED TO ADD THE S3_BUCKET STRING TO YOUR .env FILE IN THE ROOT FOLDER\n\n\n";
-    }
-    let kitaList = await axios.get(S3_BUCKET);
+    await connectToDatabase();
+    const kitaList = await KitaDetailModel.find();
+    console.log(kitaList.length);
+
     let kitasInRadius: Kita[] = [];
-    kitaList.data.map((kita: Kita) => {
+    kitaList.map((kita: Kita) => {
       const distance =
         haversine(
           { lat, lon },
@@ -31,20 +30,27 @@ export async function locationService(req: any, res: any, next: NextFunction) {
         kitasInRadius.push(kita);
       }
     });
-    const sortedKitaList = sortKitaListByDistance(kitaList.data)
-    const BACKEND_URL = process.env.VITE_BACKEND_URL
+    console.log(kitasInRadius);
+    const sortedKitaList = sortKitaListByDistance(kitasInRadius);
+    const BACKEND_URL = process.env.VITE_BACKEND_URL;
     if (!BACKEND_URL) {
-      throw "\n\n\nYOU NEED TO ADD THE BACKEND_URL STRING TO YOUR .env FILE IN THE ROOT FOLDER\n\n\n";
+      throw "\n\n\nYOU NEED TO ADD THE VITE_BACKEND_URL STRING TO YOUR .env FILE IN THE ROOT FOLDER\n\n\n";
     }
-    const linkToNextPage  = `${BACKEND_URL}/location-service/${lat}/${lon}/${radius}/${page+1}/${size}`
+    const linkToNextPage = `${BACKEND_URL}/location-service/${lat}/${lon}/${radius}/${
+      page + 1
+    }/${size}`;
 
-    const paginatedKitas = pagination(sortedKitaList, page, size, linkToNextPage)
-    if(paginatedKitas.metadata.pagesLeft === 0) {
-      paginatedKitas.metadata.nextUrl = false
+    const paginatedKitas = pagination(
+      sortedKitaList,
+      page,
+      size,
+      linkToNextPage
+    );
+    if (paginatedKitas.metadata.pagesLeft === 0) {
+      paginatedKitas.metadata.nextUrl = false;
     }
     logger.info(`Found ${kitasInRadius.length} Kitas in radius of ${radius}km`);
     return res.send(paginatedKitas);
-
   } catch (err: any) {
     logger.error(err);
     return res.status(500).json({ error: "Something went wrong" });
@@ -55,33 +61,35 @@ function sortKitaListByDistance(kitaList: Kita[]) {
   kitaList.sort((a, b) => {
     return a.coordinates.dist - b.coordinates.dist;
   });
-  return kitaList
+  return kitaList;
 }
 
-function pagination(sortedList: any[], page: number, size: number, linkToNextPage: string) {
-  const startIndex = page * size
-  const endIndex = (page * size) + size
+function pagination(
+  sortedList: any[],
+  page: number,
+  size: number,
+  linkToNextPage: string
+) {
+  const startIndex = page * size;
+  const endIndex = page * size + size;
   let itemsInPage;
-  if(endIndex > sortedList.length) {
-    itemsInPage = sortedList.slice(startIndex)
-
+  if (endIndex > sortedList.length) {
+    itemsInPage = sortedList.slice(startIndex);
+  } else {
+    itemsInPage = sortedList.slice(startIndex, endIndex);
   }
-  else {
-    itemsInPage = sortedList.slice(startIndex, endIndex)
-
-  }
-  const maxNumOfPages = Math.ceil(sortedList.length / size)
-  const pagesLeft = maxNumOfPages - page 
+  const maxNumOfPages = Math.ceil(sortedList.length / size);
+  const pagesLeft = maxNumOfPages - page;
   return {
     metadata: {
-      page: page, 
+      page: page,
       amountOfItems: sortedList.length,
-      totalPages: maxNumOfPages -1,
+      totalPages: maxNumOfPages - 1,
       itemsPerPage: size,
       returnedItems: itemsInPage.length,
-      pagesLeft: pagesLeft -1,
-      nextUrl: <string | boolean>linkToNextPage 
+      pagesLeft: pagesLeft - 1,
+      nextUrl: <string | boolean>linkToNextPage,
     },
     items: itemsInPage,
-  } 
+  };
 }
