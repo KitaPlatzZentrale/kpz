bc028058dd6835612f9b24c507ede4a1abc21216
anthony <anthony.sherrill@code.berlin>
2023-04-19 17:11:35 +0200

    Changed logger so that Datadog can better read it

--- backend/src/services/logger.ts
+++ backend/src/services/logger.ts
@@ -10,18 +10,18 @@ const consoleFormat = winston.format.combine(
   })
 );
 
+const jsonFormat = winston.format.combine(
+  winston.format.timestamp({
+    format: "YYYY-MM-DD HH:mm:ss",
+  }),
+  winston.format.json()
+);
+
 // create a new logger instance
 const logger = winston.createLogger({
   level: "info",
-  format: winston.format.combine(
-    winston.format.timestamp({
-      format: "YYYY-MM-DD HH:mm:ss",
-    }),
-    winston.format.printf(({ level, message, timestamp }) => {
-      return `${timestamp} ${level}: ${message}`;
-    })
-  ),
-  defaultMeta: { service: "kpz-backend" },
+  format: jsonFormat,
+  defaultMeta: { service: `kpz-${process.env.NODE_ENV}-backend` },
   transports: [
     new winston.transports.File({ filename: "logs/error.log", level: "error" }),
     new winston.transports.File({ filename: "logs/combined.log" }),
