dc724ab5e8d087ece94ddf451e3d5929cfbdd2a8
anthony <anthony.sherrill@code.berlin>
2023-04-19 12:28:38 +0200

    Changed logger settings so that Datadog can access them easier

--- backend/src/services/logger.ts
+++ backend/src/services/logger.ts
@@ -1,4 +1,14 @@
-import * as winston from 'winston';
+import * as winston from "winston";
+
+const consoleFormat = winston.format.combine(
+  winston.format.colorize(),
+  winston.format.timestamp({
+    format: "YYYY-MM-DD HH:mm:ss",
+  }),
+  winston.format.printf(({ level, message, timestamp }) => {
+    return `${timestamp} ${level}: ${message}`;
+  })
+);
 
 // create a new logger instance
 const logger = winston.createLogger({
@@ -11,11 +21,18 @@ const logger = winston.createLogger({
       return `${timestamp} ${level}: ${message}`;
     })
   ),
-  defaultMeta: { service: "my-app" },
+  defaultMeta: { service: "kpz-backend" },
   transports: [
     new winston.transports.File({ filename: "logs/error.log", level: "error" }),
     new winston.transports.File({ filename: "logs/combined.log" }),
+    new winston.transports.Console({
+      format: consoleFormat,
+      level: "info",
+      handleExceptions: true,
+      stderrLevels: ["error"],
+    }),
   ],
+  exitOnError: false,
 });
 
 export default logger;
