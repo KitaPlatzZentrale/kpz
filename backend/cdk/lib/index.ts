028da9d5ac28c4935b7abb1a7ea98d5e080305af
Darjusch Schrand <Darjusch.schrand@code.berlin>
2023-05-15 11:24:02 +0200

    dynamic stack name to avoid overwriting

--- backend/cdk/lib/index.ts
+++ backend/cdk/lib/index.ts
@@ -1,5 +1,7 @@
 import * as cdk from "aws-cdk-lib";
 import { AppRunnerStack } from "./app-runner-stack";
+import * as dotenv from "dotenv";
 
+dotenv.config();
 const app = new cdk.App();
-new AppRunnerStack(app, "AppRunnerStack");
+new AppRunnerStack(app, process.env.APP_RUNNER_SERVICE_NAME!);
