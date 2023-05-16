a9d13419d7e4110ace20fead334f918f6babd0b7
Hanno Grimm <hanno.grimm@code.berlin>
2023-05-16 18:14:15 +0200

    zip to index

--- email/zipByName.sh
+++ email/zipByName.sh
@@ -2,6 +2,15 @@ mkdir -p zipped
 for fname in *.*; do
     prefix=${fname%%.*}
     [ ! -f "$fname" ] || [ -f "$prefix.zip" ] && continue
-    zip "$prefix" "$prefix".*  
-    mv "$prefix".zip zipped 
+    mkdir -p "$prefix"
+    mv "$prefix".* "$prefix"
+    cd "$prefix"
+    i=0
+    for f in *.*; do
+        ext=${f##*.}
+        mv "$f" "index.$ext"
+        i=$((i+1))
+    done
+    zip -r "../zipped/$prefix.zip" .
+    cd ..
 done
