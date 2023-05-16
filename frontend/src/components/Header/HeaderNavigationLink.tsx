8295708bd0fe1b7826dee61c9083296ab181bca2
Hanno Grimm <hanno.grimm@code.berlin>
2023-05-16 11:48:41 +0200

    Adjust NavLink to navigate on :after click event

--- frontend/src/components/Header/HeaderNavigationLink.tsx
+++ frontend/src/components/Header/HeaderNavigationLink.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import clsx from "clsx";
-import { NavLink, useLocation } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 type HeaderNavigationLinkProps = React.PropsWithChildren<{
   to: string;
@@ -14,36 +14,33 @@ const HeaderNavigationLink: React.FC<HeaderNavigationLinkProps> = ({
   onClick,
   children,
 }) => {
-  const location = useLocation();
-
-  const isActive = React.useMemo(() => {
-    return active || location.pathname === to;
-  }, [location.pathname, to]);
-
   return (
-    <li
+    <NavLink
+      to={to}
       onClick={onClick}
-      className={clsx(
-        "h-full w-fit cursor-pointer after:relative after:float-left after:mt-[8px] after:h-1 after:w-full after:rounded-full after:transition-opacity active:after:transition-opacity",
-        isActive
-          ? `after:bg-primary-dark after:opacity-100`
-          : `after:bg-[#966657] after:opacity-0 hover:after:opacity-30 active:after:opacity-100`
-      )}
+      className={({ isActive }) =>
+        clsx(
+          "h-full w-fit cursor-pointer after:relative after:float-left after:mt-[8px] after:h-1 after:w-full after:rounded-full after:transition-opacity active:after:transition-opacity",
+          isActive
+            ? `after:bg-primary-dark after:opacity-100`
+            : `after:bg-[#966657] after:opacity-0 hover:after:opacity-30 active:after:opacity-100`
+        )
+      }
     >
       <NavLink
-        className={({ isActive: isActiveProp }) =>
+        to={to}
+        className={({ isActive }) =>
           clsx(
             "h-full text-4xl sm:text-5xl xl:text-base",
-            isActive || isActiveProp
+            isActive
               ? `font-extrabold text-primary-dark`
               : `font-bold text-[#966657]`
           )
         }
-        to={to}
       >
         {children}
       </NavLink>
-    </li>
+    </NavLink>
   );
 };
 
