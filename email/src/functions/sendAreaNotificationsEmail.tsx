4d70f522679ab70de298c586a685ed02de593563
Darjusch Schrand <Darjusch.schrand@code.berlin>
2023-06-15 12:58:49 +0200

    added sns for error case to emit event

--- email/src/functions/sendAreaNotificationsEmail.tsx
+++ email/src/functions/sendAreaNotificationsEmail.tsx
@@ -2,13 +2,20 @@ import * as React from "react";
 
 import sendEmail from "../sender/sendEmail";
 import { render } from "@react-email/render";
+import {
+  SNSClient,
+  SNSClientConfig,
+  PublishCommand,
+} from "@aws-sdk/client-sns";
 
 import type { Handler } from "aws-lambda";
 
 import AreaNotificationsEmail, {
   AreaNotificationsEmailProps,
 } from "../templates/areaNotifications";
+import dotenv from "dotenv";
 
+dotenv.config();
 /**
  * Lambda Function: EmailSenderHandler
  *
@@ -45,27 +52,58 @@ interface EmailProps {
 }
 
 export const handler: Handler = async (event: EmailProps, ctx) => {
-  const { email, areaDescription, consentId } = event.detail.fullDocument;
-  const to = email;
-  if (!to) throw new Error("No recipient with `to` specified");
-  if (!areaDescription)
-    throw new Error(
-      "No description with `areaDescription` specified. Aborting. This will otherwise result in messy copy."
-    );
-  if (!consentId)
-    throw new Error(
-      "No consent id with `consentId` specified. This will otherwise result in a broken opt-out link (not compliant with GDPR)."
-    );
+  const snsClientConfig: SNSClientConfig = {
+    region: "eu-central-1",
+  };
+  const SNS = new SNSClient(snsClientConfig); // Example for Amazon SNS
 
-  const body = render(
-    <AreaNotificationsEmail
-      areaDescription={areaDescription}
-      consentId={consentId}
-    />
-  );
-  await sendEmail({
-    to,
-    body,
-    subject: "Neue Anmeldungen für deine Kita",
-  });
+  try {
+    const { email, areaDescription, consentId } = event.detail.fullDocument;
+    const to = email;
+    if (!to) throw new Error("No recipient with `to` specified");
+    if (!areaDescription)
+      throw new Error(
+        "No description with `areaDescription` specified. Aborting. This will otherwise result in messy copy."
+      );
+    if (!consentId)
+      throw new Error(
+        "No consent id with `consentId` specified. This will otherwise result in a broken opt-out link (not compliant with GDPR)."
+      );
+
+    const body = render(
+      <AreaNotificationsEmail
+        areaDescription={areaDescription}
+        consentId={consentId}
+      />
+    );
+    await sendEmail({
+      to,
+      body,
+      subject: "Neue Anmeldungen für deine Kita",
+    });
+  } catch (e) {
+    console.error(e);
+    const event = {
+      detail: {
+        alarmName: "sendAreaNotificationAlarm",
+        resources: [
+          `arn:aws:lambda:eu-central-1:897331788878:function/${process.env.AWS_LAMBDA_FUNCTION_NAME}`,
+        ],
+      },
+    };
+
+    const params = {
+      Message: JSON.stringify(event),
+      TopicArn: "arn:aws:sns:eu-central-1:897331788878:5XX_ALARM_TOPIC",
+      MessageAttributes: {
+        "Content-Type": {
+          DataType: "String",
+          StringValue: "application/json",
+        },
+      },
+    };
+    const command = new PublishCommand(params);
+    SNS.send(command);
+    throw e;
+  }
 };
