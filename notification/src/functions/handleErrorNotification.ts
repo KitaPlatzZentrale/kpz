60ca5d88d5709d5963232a30ade3e1cc974e6df1
Darjusch Schrand <Darjusch.schrand@code.berlin>
2023-06-15 15:24:42 +0200

    adjusted to handle events from cloudwatch and sns

--- notification/src/functions/handleErrorNotification.ts
+++ notification/src/functions/handleErrorNotification.ts
@@ -1,6 +1,5 @@
 import { Handler } from "aws-lambda";
 import { sendSlackErrorNotification } from "../sender/sendSlackErrorNotification";
-require("dotenv").config();
 /**
  * Lambda Function: CloudWatchAlarmHandler
  *
@@ -24,16 +23,30 @@ require("dotenv").config();
 
 interface IErrorEvent {
   detail: {
+    // This format is for our CloudWatch Alarms
     alarmName: string;
     resources: string[];
   };
+  Records?: [
+    // This format is for our SNS lambda alarm
+    {
+      Sns: {
+        Message: string; // the message also contains the detail object
+      };
+    }
+  ];
 }
 
 export const handler: Handler = async (event: IErrorEvent) => {
   try {
+    // this is because we get Errors from SNS and from CloudWatch Alarms
+    if (event.Records) {
+      const message = event.Records[0].Sns.Message;
+      event = JSON.parse(message);
+    }
     const alarmName = event.detail.alarmName;
     const resources = event.detail.resources.join(", ");
-    const awsFunctionOrServiceName = resources.split("/")[-1];
+    const awsFunctionOrServiceName = resources.split("/").pop();
     const awsFunctionOrService = resources.split(":")[2];
     const cloudWatchLogs = `https://eu-central-1.console.aws.amazon.com/cloudwatch/home?region=eu-central-1#logsV2:log-groups/log-group/$252Faws$252F${awsFunctionOrService}$252F${awsFunctionOrServiceName}`;
 
@@ -42,8 +55,8 @@ export const handler: Handler = async (event: IErrorEvent) => {
     }
 
     // Construct Slack message
-    const logLink = `Logs: ${cloudWatchLogs}`;
-    const message = `*Alarm Name:* ${alarmName}\n*Resources:* ${resources}\n\n*${logLink}*`;
+    const logLink = `*Logs*: ${cloudWatchLogs}`;
+    const message = `*Alarm Name:* ${alarmName}\n*Resources:* ${resources}\n\n${logLink}`;
 
     // Send message to Slack
     await sendSlackErrorNotification(message);
