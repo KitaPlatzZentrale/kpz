bb57eee0a180addb88c5f51431cd55dbd933b8ae
Darjusch Schrand <Darjusch.schrand@code.berlin>
2023-06-15 20:40:02 +0200

    heatlhCheck lambda that sends SNS event if failed

---
+++ notification/src/functions/handleHealthCheck.ts
@@ -0,0 +1,23 @@
+import { Handler } from "aws-lambda";
+import axios from "axios";
+import { sendSNS, setupSNS } from "../sender/sendSNS";
+
+export const handler: Handler = async (event, context) => {
+  const SNS = setupSNS();
+  try {
+    // GET https://api.kitaplatz-zentrale.de/health using axios
+    const res = await axios.get("https://api.kitaplatz-zentrale.de/health");
+    if (res.status === 200) {
+      console.log("Health check success");
+    } else {
+      console.log("Health check failed");
+      sendSNS(SNS);
+
+      throw new Error("Health check failed");
+    }
+  } catch (error) {
+    console.log(error);
+    sendSNS(SNS);
+    throw error;
+  }
+};
