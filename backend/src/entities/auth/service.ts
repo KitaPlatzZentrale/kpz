9699a6dcc859bd9b91557c559f61569c4a68b1b9
Darjusch Schrand <Darjusch.schrand@code.berlin>
2023-07-07 09:31:50 +0200

    Protect /save-child, /get-child, delete user
    Use AUTH_KEY ( API key ) till user signup and login is implemented and we use JWT

--- backend/src/entities/auth/service.ts
+++ backend/src/entities/auth/service.ts
@@ -1,8 +1,15 @@
 import { RequestHandler } from "express";
-
+import dotenv from "dotenv";
+dotenv.config();
 //  middleware to check if user is authenticated
 export const isAuthenticated: RequestHandler<Request> = (req, res, next) => {
   const placeholder = true;
+  if (!req.headers["x-api-key"]) {
+    return res.status(401).json({ message: "No API key provided" });
+  }
+  if (req.headers["x-api-key"] !== process.env.AUTH_KEY) {
+    return res.status(401).json({ message: "Wrong API key provided" });
+  }
   if (placeholder) {
     return next();
   }
