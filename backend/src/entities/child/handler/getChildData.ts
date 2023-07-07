1468dbc20b1d9110ac2c27a11467a32fe3cac248
Darjusch Schrand <Darjusch.schrand@code.berlin>
2023-07-07 10:47:01 +0200

    cleaned up child handler and validator

--- backend/src/entities/child/handler/getChildData.ts
+++ backend/src/entities/child/handler/getChildData.ts
@@ -1,34 +1,7 @@
-import { IsNotEmpty, IsString, validate } from "class-validator";
 import { RequestHandler } from "express";
 import logger from "../../../logger";
 import ChildDataService from "../service";
-
-interface IGetChildData {
-  id: string;
-}
-
-class GetChildDataValidator {
-  @IsString()
-  @IsNotEmpty()
-  id: string;
-}
-
-export const validator: RequestHandler<any, any, IGetChildData> = async (
-  req,
-  res,
-  next
-) => {
-  try {
-    const validator = new GetChildDataValidator();
-    validator.id = req.params.id;
-    const errors = await validate(validator);
-    if (errors.length) return res.status(400).json({ error: errors });
-    return next();
-  } catch (e) {
-    logger.error(e);
-    return res.status(500).send("Something went wrong");
-  }
-};
+import { IGetChildData } from "../types";
 
 const handler: RequestHandler<any, any, IGetChildData> = async (req, res) => {
   try {
