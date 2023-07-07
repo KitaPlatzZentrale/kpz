99f6a5f5627014b936cafd8bc7843d9688ffd568
Darjusch Schrand <Darjusch.schrand@code.berlin>
2023-07-07 10:47:16 +0200

    cleaned up kita handler and validator

--- backend/src/entities/kitas/handler.ts
+++ backend/src/entities/kitas/handler.ts
@@ -1,65 +1,9 @@
 import logger from "../../logger";
-import {
-  IsInt,
-  IsLatitude,
-  IsLongitude,
-  IsNumber,
-  validate,
-} from "class-validator";
 import KitaService from "./service";
 import { RequestHandler } from "express";
-
-require("dotenv").config();
-
-export interface IPaginatedKitasParams {
-  lat: string;
-  lng: string;
-  radius: string;
-  page?: string;
-  limit?: string;
-}
-
-export class PaginatedKitasParams {
-  @IsLatitude()
-  lat: number;
-
-  @IsLongitude()
-  lng: number;
-
-  @IsNumber()
-  radius: number;
-
-  @IsInt()
-  page?: number;
-
-  @IsInt()
-  limit?: number;
-}
-
-export const validator: RequestHandler<IPaginatedKitasParams> = async (
-  req,
-  res,
-  next
-) => {
-  try {
-    const newLocation = new PaginatedKitasParams();
-
-    newLocation.lat = Number(req.params.lat);
-    newLocation.lng = Number(req.params.lng);
-    newLocation.radius = Number(req.params.radius || 2500);
-    newLocation.page = Number(req.params.page || 1);
-    newLocation.limit = Number(req.params.limit || 50);
-
-    const errors = await validate(newLocation);
-
-    if (errors.length) return res.status(400).json({ error: errors });
-
-    return next();
-  } catch (err: any) {
-    logger.error(err);
-    return res.status(500).json({ error: "Something went wrong" });
-  }
-};
+import { IPaginatedKitasParams } from "./types";
+import dotenv from "dotenv";
+dotenv.config();
 
 const handler: RequestHandler<IPaginatedKitasParams> = async (req, res) => {
   try {
