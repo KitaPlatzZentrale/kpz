677ab2756594f16f1e2a22589434aef4a14fe23c
Darjusch Schrand <Darjusch.schrand@code.berlin>
2024-05-05 23:37:46 +0200

    gtm

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -12,6 +12,12 @@ import router from "./router";
 import "./styles/index.css";
 import theme from "./theme";
 import ReactGA from "react-ga4";
+import TagManager from "react-gtm-module";
+const tagManagerArgs = {
+  gtmId: "GTM-NPXTHLVJ",
+};
+TagManager.initialize(tagManagerArgs);
+
 const TRACKING_ID = "G-GNDE5XDS4C";
 ReactGA.initialize(TRACKING_ID);
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
