c5df9e12669aa0278039b212cf3d046d7476c958
5h3rr1ll <a@sherrill.de>
2026-04-09 11:22:44 +0200

    fix: Correct NAT Gateway reference in private route
    Update the nat_gateway_id to reference a single resource directly instead of using an index.

--- terraform/modules/networking/vpc/main.tf
+++ terraform/modules/networking/vpc/main.tf
@@ -82,7 +82,7 @@ resource "aws_route" "public_internet_access" {
 resource "aws_route" "private_internet_access" {
   route_table_id         = aws_route_table.private.id
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.main[0].id
+  nat_gateway_id         = aws_nat_gateway.main.id
 }
 
 resource "aws_route_table_association" "public" {
